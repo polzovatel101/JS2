fea461daa3432b19045879e0b8bc2b25a17b7690
Илья Яновой <polzovatel101@mail.ru>
2016-03-29 15:04:17 +0300

    JS2 HW upd

--- js/script.js
+++ js/script.js
@@ -10,26 +10,47 @@ var Tests = {
     },
 
     test: {
-        question: 'Вопрос №',
-        answer: 'Вариант ответа №'
+        question1: 'Вопрос №1',
+        question2: 'Вопрос №2',
+        question3: 'Вопрос №3',
+        answer1: 'Вариант ответа №1',
+        answer2: 'Вариант ответа №2',
+        answer3: 'Вариант ответа №3'
     },
 
     createTreeText: function (obj) {
         var li = '';
         var container = document.querySelector('body');
         container.innerHTML += '<h2>' + 'Тест по программированию' + '</h2>' + '\n' + '<form>';
-        for(var i = 1; i <= 3; i++)
-        {
-            container.innerHTML += '<ul>' + obj.question + i + '</ul>';
-            for(var j = 1; j <= 3; j++)
-            {
-                container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer + j + '</input>' + '</label>' + '</li>' + '</label>';
-            }
-        }
+        //for(var i = 1; i <= 3; i++)
+        //{
+            container.innerHTML += '<ul>' + obj.question1 + '</ul>';
+            //for(var j = 1; j <= 3; j++)
+            //{
+                container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer1 + '</input>' + '</label>' + '</li>' + '</label>';
+                container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer2 + '</input>' + '</label>' + '</li>' + '</label>';
+                container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer3 + '</input>' + '</label>' + '</li>' + '</label>';
+            //}
+
+        container.innerHTML += '<ul>' + obj.question2 + '</ul>';
+        //for(var j = 1; j <= 3; j++)
+        //{
+        container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer1 + '</input>' + '</label>' + '</li>' + '</label>';
+        container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer2 + '</input>' + '</label>' + '</li>' + '</label>';
+        container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer3 + '</input>' + '</label>' + '</li>' + '</label>';
+        //}
+        container.innerHTML += '<ul>' + obj.question3 + '</ul>';
+        //for(var j = 1; j <= 3; j++)
+        //{
+        container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer1 + '</input>' + '</label>' + '</li>' + '</label>';
+        container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer2 + '</input>' + '</label>' + '</li>' + '</label>';
+        container.innerHTML += '<li>' + '<label>' + '<input>' + obj.answer3 + '</input>' + '</label>' + '</li>' + '</label>';
+        //}
+        //}
         container.innerHTML += '<button>' + 'Проверить мои результаты' + '</button>' + '\n' + '</form>';
         var input = document.querySelectorAll('input');
         console.log(input);
-        for(i = 0; i < input.length; i++){
+        for(var i = 0; i < input.length; i++){
             input[i].type = 'checkbox';
         }
         var button = document.querySelector('button');
